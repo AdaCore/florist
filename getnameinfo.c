cbd8a8d7ce3db0ab22db43af212f6847d6697958
Arnaud Charlet <charlet@adacore.com>
1998-09-29 11:02:25 +0000

    Remove junk revision history.

--- getnameinfo.c
+++ getnameinfo.c
@@ -120,15 +120,3 @@ do_ipv46(char *host, size_t hostlen, char *serv, size_t servlen,
 	}
 	return(0);
 }
-#if 0
-----------------------
--- REVISION HISTORY --
-----------------------
-
-----------------------------
-revision 1.1  locked by: baker;
-date: 1997/11/03 12:16:58;  author: baker;  state: Exp;
-Integrated
----------------------------------
-** New changes after this line and before endif. **
-#endif
