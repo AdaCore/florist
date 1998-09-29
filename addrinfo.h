f1c96c75516e699445971974104d4417503a6063
Arnaud Charlet <charlet@adacore.com>
1998-09-29 08:56:22 +0000

    Remove junk revision history

--- addrinfo.h
+++ addrinfo.h
@@ -40,15 +40,3 @@ void freeaddrinfo(struct addrinfo *);
 int getnameinfo(const struct sockaddr *, size_t, char *, size_t,
 				     char *, size_t);
 #endif	/* __addrinfo_h */
-#if 0
-----------------------
--- REVISION HISTORY --
-----------------------
-
-----------------------------
-revision 1.1  locked by: baker;
-date: 1997/11/03 12:16:58;  author: baker;  state: Exp;
-Integrated
----------------------------------
-** New changes after this line and before endif. **
-#endif
