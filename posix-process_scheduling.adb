0ec7112985a9986a2b936f4039ee15005ba9ad92
Arnaud Charlet <charlet@adacore.com>
1998-10-19 14:41:43 +0000

    (Get_Scheduling_Parameters): correct typo sched_setparam -> sched_getparam

--- posix-process_scheduling.adb
+++ posix-process_scheduling.adb
@@ -102,7 +102,7 @@ package body POSIX.Process_Scheduling is
      return Scheduling_Parameters is
       Params : aliased Scheduling_Parameters;
    begin
-      Check (sched_setparam
+      Check (sched_getparam
         (To_pid_t (Process), Params.Param'Unchecked_Access));
       return Scheduling_Parameters (Params);
    end Get_Scheduling_Parameters;
