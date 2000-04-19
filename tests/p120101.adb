94e9c7777e3876a205a39070a8283a459adab5d5
Arnaud Charlet <charlet@adacore.com>
2000-04-19 12:57:30 +0000

    Correct pathname of child process. Correct recovery from Storage_Error. Correct Time_Big_List_Access to recover storage after hitting Storage_Error, so that later iterations to not falsely run out  of storage. Remove dependence on support for optional POSIX real-time clock feature. Added some comments at the beginning make the Assert number "Axxx" in order. Sync with FSU.

--- tests/p120101.adb
+++ tests/p120101.adb
@@ -7,7 +7,7 @@
 --                                B o d y                                   --
 --                                                                          --
 --                                                                          --
---  Copyright (c) 1995-1998 Florida  State  University  (FSU).  All Rights  --
+--  Copyright (c) 1995-1999 Florida  State  University  (FSU).  All Rights  --
 --  Reserved.                                                               --
 --                                                                          --
 --  This is free software;  you can redistribute it and/or modify it under  --
@@ -42,8 +42,8 @@
 
 --  This test is on POSIX_Memory_Locking. The test generate memory-bound
 --  processes, and get performance results on memory access with lock and
---  without. Thus this test also covers following "Untestable Aspects" in VSRT
---  Assertion set:
+--  without. Thus this test tries to covers the following "Untestable Aspects"
+--  in the VSRT:
 
 --  rt.os/mlock/mlockall/Untestable_Aspects/1:
 --    A call to mlockall() when flags contains MCL_CURRENT shall cause all of
@@ -54,22 +54,34 @@
 --    A call to munlockall() shall unlock all currently mapped pages of the
 --    address space of the process
 
+--  Setup: This program requires that its own executable be accessible
+--  via pathname "./p120101", in order create a child process.
 
-with POSIX,
+--  The test strategy is to measure the difference in performance of
+--  the main process doing a memory-intensive computation both with and
+--  without memory locking.  If the performance is better with memory
+--  locking, that indicates it had some effect.  If the performance is
+--  unchanged, the result is inconclusive.
+
+--  For the test to work well you will need a machine with limited
+--  real memory and will need to run it with unlimited "ulimit" values.
+
+with Ada.Calendar,
+     POSIX,
      POSIX_Limits,
      POSIX_Memory_Locking,
      POSIX_Process_Environment,
      POSIX_Process_Identification,
      POSIX_Process_Primitives,
      POSIX_Signals,
-     POSIX_Timers,
-     POSIX_Report;
+     POSIX_Report,
+     Unchecked_Deallocation;
 
 procedure p120101 is
 
-   use POSIX,
+   use Ada.Calendar,
+       POSIX,
        POSIX_Memory_Locking,
-       POSIX_Timers,
        POSIX_Report;
 
    Array_Size : Natural := POSIX_Limits.Page_Size_Range'Last / Natural'Size;
@@ -81,6 +93,27 @@ procedure p120101 is
       Pad : Array_Type;
       Next : Pointer_Type;
    end record;
+
+   Head : Pointer_Type;
+   Delay_Time : constant Duration := 10.0;
+   --  enough time for other processes to access their entire
+   --  lists, causing the pages to be brought into local memory
+   My_Path : POSIX.POSIX_String := "./p120101";
+   Num_Processes : Natural := 5;
+
+   type Process_Record is record
+      Child : POSIX_Process_Identification.Process_ID;
+      Pathname : POSIX.Pathname (1 .. My_Path'Length) := My_Path;
+      Template : POSIX_Process_Primitives.Process_Template;
+      Arg_List : POSIX.POSIX_String_List;
+   end record;
+   Process_Array : array (1 .. Num_Processes) of Process_Record;
+
+   List_Size : Integer := 1024;
+
+   We_Are_The_Child : Boolean := False;
+
+   procedure Free is new Unchecked_Deallocation (Record_Type, Pointer_Type);
 
    --------------------------
    -- Time_Big_List_Access --
@@ -96,7 +129,7 @@ procedure p120101 is
       P1 : Pointer_Type;
       Sum : Duration := 0.0;
       Reps : constant Integer := 1;
-      Time1, Time2 : POSIX.Timespec;
+      Time1, Time2 : Ada.Calendar.Time;
       Dur : Duration;
    begin
       Comment ("Time_Big_List_Access " & Integer'Image (Size));
@@ -108,35 +141,58 @@ procedure p120101 is
       end loop;
       for J in 1 .. Reps loop
          P1 := Head;
-         Time1 := Get_Time (Clock_Realtime);
+         Time1 := Ada.Calendar.Clock;
          --  This loop should not need to bring the list back from secondary
          --  storage, unless it was too big to fit into primary memory.
          while P1 /= null loop
             P1.Item := 1;
             P1 := P1.Next;
          end loop;
-         Time2 := Get_Time (Clock_Realtime);
-         Sum := Sum + (To_Duration (Time2 - Time1));
+         Time2 := Ada.Calendar.Clock;
+         Sum := Sum + (Time2 - Time1);
       end loop;
       Dur := Sum / Reps;
-      Comment ("Duration: "
+      Comment ("Duration:          "
         & Integer'Image (Integer (Dur * 1_000_000)) & "us");
+      while Head /= null loop
+         P1 := Head; Head := P1.Next; Free (P1);
+      end loop;
       return Dur;
    exception
-   when Storage_Error => raise;
+   when Storage_Error =>
+      while Head /= null loop
+         P1 := Head; Head := P1.Next; Free (P1);
+      end loop;
+      raise;
    when E : others  => Unexpected_Exception (E, "Time_Big_List_Access");
       return Dur;
    end Time_Big_List_Access;
+
+   -------------------
+   -- Traverse_List --
+   -------------------
+
+   procedure Traverse_List;
+
+   procedure Traverse_List is
+      P1 : Pointer_Type;
+   begin
+      P1 := Head;
+      while P1 /= null loop
+         P1.Item := 1;
+         P1 := P1.Next;
+      end loop;
+   end Traverse_List;
 
    ---------------------------
    -- Time_Test_List_Access --
    ---------------------------
 
+   --  Compute the average amount of time it takes to traverse a
+   --  given linked list of integers.
+
    function Time_Test_List_Access (Head : Pointer_Type; N : Integer)
       return Duration;
-
-   --  Compute the average amount of time it takes to access a
-   --  given linked list of integers.
 
    function Time_Test_List_Access
      (Head : Pointer_Type;
@@ -145,22 +201,17 @@ procedure p120101 is
       Sum  : Duration := 0.0;
       Reps : constant Integer := 1;
       D    : Duration;
-      P1   : Pointer_Type;
-      Time1, Time2 : POSIX.Timespec;
+      Time1, Time2 : Ada.Calendar.Time;
    begin
       Comment ("Time_Test_List_Access " & Integer'Image (N));
-      P1 := Head;
       for J in 1 .. Reps loop
-         Time1 := Get_Time (Clock_Realtime);
+         Time1 := Ada.Calendar.Clock;
          --  The following loop  will access the nodes of this list.  The
          --  nodes should not be in secondary storage, unless the list was
          --  too big to fit into primary memory.
-         while P1 /= null loop
-            P1.Item := 1;
-            P1 := P1.Next;
-         end loop;
-         Time2 := Get_Time (Clock_Realtime);
-         Sum := Sum + (To_Duration (Time2 - Time1));
+         Traverse_List;
+         Time2 := Ada.Calendar.Clock;
+         Sum := Sum + (Time2 - Time1);
       end loop;
       D := Sum / Reps;
       Comment ("Duration: " &
@@ -168,37 +219,33 @@ procedure p120101 is
       return D;
    end Time_Test_List_Access;
 
+   ----------------
+   --  Check_Arg --
+   ----------------
 
-   Delay_Time : constant Duration := 30.0;
-   Head : Pointer_Type;
-   My_Path : POSIX.POSIX_String := "test_memory_locking_b";
-   Num_Processes : Natural := 5;
-
-   type Process_Record is record
-      Child : POSIX_Process_Identification.Process_ID;
-      Pathname : POSIX.Pathname (1 .. My_Path'Length) := My_Path;
-      Template : POSIX_Process_Primitives.Process_Template;
-      Arg_List : POSIX.POSIX_String_List;
-   end record;
-   Process_Array : array (1 .. Num_Processes) of Process_Record;
-
-   List_Size : Integer := 1024;
-
-   We_Are_The_Child : Boolean := False;
    procedure Check_Arg (Item : in POSIX_String; Quit : in out Boolean);
+
    procedure Check_Arg (Item : in POSIX_String; Quit : in out Boolean) is
    begin
       if Item = "-b" then We_Are_The_Child := True;
          Quit := True;
       end if;
    end Check_Arg;
+
    procedure Check_Args is new For_Every_Item (Check_Arg);
 
+   -------------------
+   -- Set_List_Size --
+   -------------------
+
    procedure Set_List_Size (List_Size : in out Integer);
+
    procedure Set_List_Size (List_Size : in out Integer) is
       D1, D2 : Duration;
    begin
-      Comment ("Estimate size needed to force paging behavior");
+      if not We_Are_The_Child then
+         Comment ("Estimate size needed to force paging behavior");
+      end if;
       Outer :
       loop
          begin
@@ -214,7 +261,7 @@ procedure p120101 is
                   return;
                end if;
                List_Size := List_Size * 3 / 2;
-               exit when D2 > D1 * 1.5;
+               exit when D2 > D1 * 2;
                D1 := D2;
             end loop;
             return;
@@ -222,13 +269,16 @@ procedure p120101 is
          when Storage_Error =>
             Comment ("Reducing list size due to Storage_Error");
             loop
-               List_Size := List_Size / 2;
                begin
                   D1 := Time_Big_List_Access (List_Size);
                   exit Outer;
                exception
                when Storage_Error => null;
+                  if List_Size = 0 then
+                     Fatal ("A001: Storage_Error for zero list size");
+                  end if;
                end;
+               List_Size := List_Size * 7 / 8;
             end loop;
          when E : others =>
             Unexpected_Exception  (E, "exception estimating size needed" &
@@ -237,66 +287,41 @@ procedure p120101 is
       end loop Outer;
    end Set_List_Size;
 
-begin
+   -----------------
+   -- Create_List --
+   -----------------
 
-   --  Scan for appropriate arguments, and begin doing background work
-   --  to keep memory busy if we are not the primary process.
+   procedure Create_List;
 
-   Check_Args (POSIX_Process_Environment.Argument_List);
-   if We_Are_The_Child then
-      --  We are a child process, who is supposed to keep the
-      --  system busy with memory accesses.
-      --  This loop does not need to terminate, because our
-      --  parent will kill us when the test is done.
-      Set_List_Size (List_Size);
-      declare
-         N : Integer := 1024;
-         D1 : Duration;
-         D2 : Duration := 0.0;
-      begin
-         D1 := Time_Big_List_Access (N);
-         D2 := Time_Big_List_Access (N * 3 / 2);
-         loop
-            if D2 >  D1 * 1.5 then
-               loop
-                  D2 := Time_Big_List_Access (N * 3 / 2);
-               end loop;
-            else N := N * 3 / 2;
-            end if;
-         end loop;
-      exception
-      when E : others => Unexpected_Exception (E, "A001");
-      end;
-   end if;
-
-   --  If we get this far, we are the primary process for this test.
-
-   Header ("p120101", Root_OK => True);
-   Test ("Memory_Locking Performance Test");
-
-   Set_List_Size (List_Size);
-
-   Comment ("Creating list of size " & Integer'Image (List_Size));
-   Head := new Record_Type;
-   declare
+   procedure Create_List is
       P : Pointer_Type := Head;
-      D : Duration;
    begin
+      if not We_Are_The_Child then
+         Comment ("Creating list of size " & Integer'Image (List_Size));
+      end if;
+      Head := new Record_Type;
+      P := Head;
       for I in 2 .. List_Size loop
          P.Next := new Record_Type;
          P := P.Next;
       end loop;
-      D := Time_Test_List_Access (Head, List_Size);
-   end;
+   end Create_List;
 
+   --------------------
+   -- Randomize_List --
+   --------------------
+
+   procedure Randomize_List;
+
+   procedure Randomize_List is
    --  Try to sufficiently randomize the list so that we minimize the
    --  pre-paging effects of the system.
-
-   declare
       P1, P2, Tmp1, Tmp2, Last : Pointer_Type;
    begin
       for K in 1 .. 7 loop
-         Comment ("Starting shuffle number " & Integer'Image (K) & ".");
+         if not We_Are_The_Child then
+            Comment ("Starting shuffle number " & Integer'Image (K) & ".");
+         end if;
          P1 := Head;
          P2 := Head;
          for J in 1 .. List_Size / 2 loop
@@ -313,10 +338,47 @@ begin
             P2 := Tmp2;
          end loop;
       end loop;
-   end;
+   end Randomize_List;
 
-   Comment ("Locking all of current memory...");
-   Lock_All (Current_Pages);
+begin
+
+   --  Scan for appropriate arguments, and begin doing background work
+   --  to keep memory busy if we are not the primary process.
+
+   Check_Args (POSIX_Process_Environment.Argument_List);
+
+   if not We_Are_The_Child then
+      Header ("p120101", Root_OK => True);
+      Test ("Memory_Locking Performance Test");
+   end if;
+
+   Optional (Memory_Locking_Option, "");
+
+   --  Find a list size that seems to provoke paging, if that is
+   --  possible within the operative process resource limits.
+
+   Set_List_Size (List_Size);
+
+   --  Create a list of that size.
+
+   Create_List;
+
+   --  Randomize the order of items in the list, to reduce the
+   --  benefits of any prepaging that the sytem might do.
+
+   Randomize_List;
+
+   if We_Are_The_Child then
+      --  We are a child process, who is supposed to keep the
+      --  system busy with memory accesses.
+      --  This loop does not need to terminate, because our
+      --  parent will kill us when the test is done.
+      loop
+         Traverse_List;
+      end loop;
+   end if;
+
+   --  If we get this far, we are the primary process for this test.
 
    Comment ("Starting other processes.");
    begin
@@ -331,18 +393,16 @@ begin
             Process_Array (P).Arg_List);
          Comment ("Process " & Integer'Image (P) & " started.");
       end loop;
-      exception when E : POSIX_Error =>
-         --  If the system can't allow us to have another process, we will
-         --  go on with what we have.
-         if Get_Error_Code = Resource_Temporarily_Unavailable or
-           Get_Error_Code = Not_Enough_Space or
-           Get_Error_Code = Operation_Not_Supported then
-            Comment ("Unable to start process.  Continuing.");
-         else Unexpected_Exception (E, "A002");
-         end if;
+   exception when E : POSIX_Error =>
+      --  If the system can't allow us to have another process, we will
+      --  go on with what we have.
+      if Get_Error_Code = Resource_Temporarily_Unavailable or
+        Get_Error_Code = Not_Enough_Space or
+        Get_Error_Code = Operation_Not_Supported then
+         Comment ("Unable to start process.  Continuing.");
+      else Unexpected_Exception (E, "A002");
+      end if;
    end;
-
-   delay Delay_Time;
 
    --  Time the list accesses, both with memory locked and with memory
    --  unlocked.  Compare the results, and report to the user.
@@ -350,22 +410,35 @@ begin
    declare
       Locked_Duration, Unlocked_Duration, Ratio : Duration;
    begin
+      delay Delay_Time;
+      Comment ("Time the list access with memory unlocked.");
+      Unlocked_Duration := Time_Test_List_Access (Head, List_Size);
+      --  Do it again, because the first time may have had extra
+      --  overhead to initialize the pages.
+      Unlocked_Duration := Time_Test_List_Access (Head, List_Size);
+      Lock_All (Current_Pages);
+      delay Delay_Time;
       Comment ("Time the list access with memory locked.");
       Locked_Duration := Time_Test_List_Access (Head, List_Size);
+      --  This should take less time if locking memory saved us any
+      --  page faults.
+      Ratio := Unlocked_Duration / Locked_Duration;
+      if Ratio > 4.0 then
+         Comment ("Performance indicates that memory was locked.");
+      else
+         Comment ("Performance does not indicate expected effect.");
+      end if;
       Unlock_All;
       delay Delay_Time;
-      Comment ("Time this list access again with the memory unlocked.");
+      Comment ("Time the list access again with the memory unlocked.");
       Unlocked_Duration := Time_Test_List_Access (Head, List_Size);
+      --  This should take more time if unlocking memory caused us
+      --  to have page faults.
       Ratio := Unlocked_Duration / Locked_Duration;
-      Comment ("With memory unlocked, the test took "
-        & Integer'Image (Integer (Ratio))
-        & " times longer than with memory locked.");
-      if Ratio > 20.0 then
-         Comment ("Performance indicates that memory was locked.");
-      elsif Ratio > 10.0 then
-         Comment ("Performance does not indicate whether memory was locked.");
+      if Ratio > 4.0 then
+         Comment ("Performance indicates that memory was unlocked.");
       else
-         Comment ("Performance indicates that memory was not locked.");
+         Comment ("Performance does not indicate expected effect.");
       end if;
    end;
 
@@ -382,10 +455,12 @@ begin
       end loop;
    end;
    Done;
+
 exception
 when E1 : POSIX.POSIX_Error =>
-   --  since this is the first lock operation, we
-   --  assume there is no capacity limit
+   --  Since there is only one lock operation, we
+   --  assume there is no capacity limit, so any failures can only
+   --  be due to lack of support or lack of privilege.
    if Get_Error_Code = Resource_Temporarily_Unavailable then
       Comment ("The system was not able to lock the memory.");
    else
