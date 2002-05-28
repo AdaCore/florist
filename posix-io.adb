7dc9ba5fd9e4cb597c6144c56d701e831e7f52d7
Matthew Gingell <gingell@adacore.com>
2002-05-28 11:14:49 +0000

    Duplicate: Add pragma Unreferenced for Target.

--- posix-io.adb
+++ posix-io.adb
@@ -206,6 +206,7 @@ package body POSIX.IO is
      (File   : File_Descriptor;
       Target : File_Descriptor := 0)
      return File_Descriptor is
+      pragma Unreferenced (Target);
    begin
       return File_Descriptor (Check (dup (int (File))));
    end Duplicate;
