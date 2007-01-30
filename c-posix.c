f6fd8affd4518c05a0aa911b90da5971fd821de8
Arnaud Charlet <charlet@adacore.com>
2007-01-30 11:48:21 +0000

    Merge G124-015: fix support for sparc64 and solaris 10. Remove obsolete files.

--- c-posix.c
+++ c-posix.c
@@ -1300,12 +1300,13 @@ struct addrinfo {
   GT2(ai_protocol, int)
 
 /* Workaround layout issue on Sparc64 version 10. */
-#if defined (__sun__) && defined(__arch64__) && !defined(_GCC_SIZE_T)
+#if defined (__solaris10__) && defined (__arch64__)
   GT2(_ai_pad, int)
   GT2(ai_addrlen, int)
 #else
   GT2(ai_addrlen, size_t)
 #endif
+
   GT2(ai_addr, struct sockaddr *)
   GT2(ai_canonname, char *)
   GT2(ai_next, struct addrinfo *)
