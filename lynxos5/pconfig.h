a830cc130b7ed040608b1d583bb53a12f589886c
Arnaud Charlet <charlet@adacore.com>
2013-01-23 22:34:35 +0000

    Add pre-generated files for lynxos 5 as a work around for lynxos customers who need to use Florist. This is experimental for now and kludgy. Part of M123-004

---
+++ lynxos5/pconfig.h
@@ -0,0 +1,9 @@
+/* file: pconfig.h.in
+   LynxOS version
+ */
+#define _POSIX_C_SOURCE 199506L
+#define _REENTRANT
+#define _POSIX_C_SIGNALS_C
+#define _BSD_SOURCE
+#include <semaphore.h>
+#include <arpa/inet.h>
